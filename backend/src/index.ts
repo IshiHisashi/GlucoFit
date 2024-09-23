5d38eb3f25c829215fd696b68699fb77736aea5e
Shehani Wijetunga <77097408+shehani-wijetunga@users.noreply.github.com>
2024-09-23 16:34:20 -0700

    Adding MongoDB connection
    1. Created cluster in Atlas MongoDB.
    2.Conected by adding url to .env
    3.Add connection code to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,32 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// MongoDB connection
+const mongoURI = process.env.MONGODB_URI;
+
+if (mongoURI) {
+
+  console.log("MongoDB URI: ", mongoURI);
+  mongoose
+    .connect(mongoURI)
+    .then(() => {
+      console.log("Connected to MongoDB successfully");
+    })
+    .catch((err) => {
+      console.error("Failed to connect to MongoDB", err);
+    });
+} else {
+  console.error("MONGODB_URI is not defined in the .env file");
+}
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript + MongoDB Server");
 });
 
 app.listen(port, () => {
