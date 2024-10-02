4374d269901050469156b99f45f2ed8e7c83793d
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-10-02 08:18:33 -0700

    feat: JDEV-55 - add elements on glucose log screen

--- frontend/src/components/screens/logScreens/GlucoseLogScreen.tsx
+++ frontend/src/components/screens/logScreens/GlucoseLogScreen.tsx
@@ -1,11 +1,107 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import {
+  ButtonText,
+  Center,
+  FormControl,
+  FormControlLabelText,
+  HStack,
+  Image,
+  Input,
+  InputField,
+  InputIcon,
+  Text,
+  VStack,
+} from "@gluestack-ui/themed";
 import React from "react";
+
+import GlucoFitFaceSample from "../../../../assets/GlucoFit-Face-sample.png";
+import { FormControlLabel } from "@gluestack-ui/themed";
+import { Textarea } from "@gluestack-ui/themed";
+import { TextareaInput } from "@gluestack-ui/themed";
+import { Button } from "@gluestack-ui/themed";
+import { InputSlot } from "@gluestack-ui/themed";
 
 const GlucoseLogScreen: React.FC = () => {
   return (
-    <Center>
-      <Text>This is glucose log screen.</Text>
-    </Center>
+    <VStack>
+      <Center>
+        <Text>This is glucose log screen.</Text>
+      </Center>
+
+      <Center>
+        <Image source={GlucoFitFaceSample} alt="GlucoFit face" />
+      </Center>
+
+      <FormControl isRequired>
+        <Input>
+          <InputField
+            type="text"
+            placeholder="Input your glucose level..."
+            onChangeText={() => {}}
+            // value={}
+          />
+          <InputSlot pr="$3" onPress={() => {}}>
+            <Text>mg/dL</Text>
+          </InputSlot>
+        </Input>
+      </FormControl>
+
+      <FormControl isRequired>
+        <HStack>
+          <FormControlLabel>
+            <FormControlLabelText>Time</FormControlLabelText>
+          </FormControlLabel>
+          <Input>
+            <InputField
+              type="text"
+              placeholder="date..."
+              onChangeText={() => {}}
+              // value={}
+            />
+          </Input>
+          <Input>
+            <InputField
+              type="text"
+              placeholder="time..."
+              onChangeText={() => {}}
+              // value={}
+            />
+          </Input>
+        </HStack>
+      </FormControl>
+
+      <FormControl isRequired>
+        <HStack>
+          <FormControlLabel>
+            <FormControlLabelText>Time Period</FormControlLabelText>
+          </FormControlLabel>
+          <Input>
+            <InputField
+              type="text"
+              placeholder="time period..."
+              onChangeText={() => {}}
+              // value={}
+            />
+          </Input>
+        </HStack>
+      </FormControl>
+
+      <FormControl isRequired>
+        <VStack>
+          <FormControlLabel>
+            <FormControlLabelText>Additional Notes</FormControlLabelText>
+          </FormControlLabel>
+          <Textarea>
+            <TextareaInput placeholder="Additional Notes Here" />
+          </Textarea>
+        </VStack>
+      </FormControl>
+
+      <FormControl>
+        <Button onPress={() => {}}>
+          <ButtonText>Save</ButtonText>
+        </Button>
+      </FormControl>
+    </VStack>
   );
 };
 
