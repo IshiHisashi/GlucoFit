c552d3e6ee4f5e3cc7210912915c73602c82a125
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-10-04 22:42:55 -0700

    feat: JDEV-62 - add createTestResult mutation

--- frontend/src/components/screens/logScreens/GlucoseLogScreen.tsx
+++ frontend/src/components/screens/logScreens/GlucoseLogScreen.tsx
@@ -12,15 +12,83 @@ import {
   VStack,
 } from "@gluestack-ui/themed";
 import React from "react";
-
-import GlucoFitFaceSample from "../../../../assets/GlucoFit-Face-sample.png";
 import { FormControlLabel } from "@gluestack-ui/themed";
 import { Textarea } from "@gluestack-ui/themed";
 import { TextareaInput } from "@gluestack-ui/themed";
 import { Button } from "@gluestack-ui/themed";
 import { InputSlot } from "@gluestack-ui/themed";
+import { gql, useQuery, useMutation } from "@apollo/client";
+
+import GlucoFitFaceSample from "../../../../assets/GlucoFit-Face-sample.png";
+
+const GET_TEST_RESULTS = gql`
+  query GetTestResults {
+    getTestResults {
+      bsl
+      confirmed
+      id
+      log_timestamp
+      note {
+        note_description
+      }
+    }
+  }
+`;
+
+const CREATE_TEST_RESULT = gql`
+  mutation CreateTestResult(
+    $user_id: ID!
+    $bsl: Float!
+    $note: NoteInput!
+    $log_timestamp: Date!
+    $confirmed: Boolean!
+  ) {
+    createTestResult(
+      user_id: $user_id
+      bsl: $bsl
+      note: $note
+      log_timestamp: $log_timestamp
+      confirmed: $confirmed
+    ) {
+      id
+      bsl
+      note {
+        note_description
+      }
+      log_timestamp
+      confirmed
+    }
+  }
+`;
 
 const GlucoseLogScreen: React.FC = () => {
+  const [createTestResult, { data, loading, error }] =
+    useMutation(CREATE_TEST_RESULT);
+
+  const handleSubmit = async () => {
+    try {
+      const result = await createTestResult({
+        variables: {
+          user_id: "60d8f33e7f3f83479cbf5b4f", // hardcode for now
+          bsl: 95.4,
+          note: {
+            note_description: "Fasting blood sugar before breakfast",
+          },
+          log_timestamp: new Date().toISOString(),
+          confirmed: true,
+        },
+      });
+      console.log("Mutation result:", result);
+    } catch (e) {
+      console.error("Error creating test result:", e);
+    }
+  };
+
+  // const { loading, error, data } = useQuery(GET_TEST_RESULTS);
+  // if (loading) return "Loading...";
+  // if (error) return `Error! ${error.message}`;
+  // console.log(data);
+
   return (
     <VStack>
       <Center>
@@ -97,7 +165,7 @@ const GlucoseLogScreen: React.FC = () => {
       </FormControl>
 
       <FormControl>
-        <Button onPress={() => {}}>
+        <Button onPress={handleSubmit}>
           <ButtonText>Save</ButtonText>
         </Button>
       </FormControl>
