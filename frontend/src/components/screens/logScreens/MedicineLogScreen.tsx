bdcda93d8cf9cd6a8d59dba96e3c73de9637c9e2
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-10-11 12:24:03 -0700

    style: JDEV-68 - fix button placement

--- frontend/src/components/screens/logScreens/MedicineLogScreen.tsx
+++ frontend/src/components/screens/logScreens/MedicineLogScreen.tsx
@@ -6,6 +6,8 @@ import {
   ScrollView,
   Text,
   VStack,
+  View,
+  Box,
 } from "@gluestack-ui/themed";
 import React, { useState } from "react";
 import ListCard from "../../molcules/ListCard";
@@ -14,6 +16,13 @@ import ListCard from "../../molcules/ListCard";
 const medicines = [
   { name: "Apotokishin", dosage: "16mg", frequency: "Everyday" },
   { name: "Kakkonto", dosage: "16mg", frequency: "Everyday" },
+  { name: "Advil", dosage: "16mg", frequency: "Everyday" },
+  { name: "Advil2", dosage: "16mg", frequency: "Everyday" },
+  { name: "Advil3", dosage: "16mg", frequency: "Everyday" },
+  { name: "Advil4", dosage: "16mg", frequency: "Everyday" },
+  { name: "Advil5", dosage: "16mg", frequency: "Everyday" },
+  { name: "Advil6", dosage: "16mg", frequency: "Everyday" },
+  { name: "Advil7", dosage: "16mg", frequency: "Everyday" },
 ];
 
 const MedicineLogScreen: React.FC = () => {
@@ -32,26 +41,39 @@ const MedicineLogScreen: React.FC = () => {
   };
 
   return (
-    <ScrollView p="$4">
-      <VStack space="md">
-        {medicines &&
-          medicines.map((obj) => (
-            <ListCard
-              text={obj.name}
-              isSelected={selectedMeds.includes(obj.name)}
-              iconLeft={CalendarDaysIcon}
-              iconRightOn={CalendarDaysIcon}
-              iconRightOff={SunIcon}
-              badge={[obj.dosage, obj.frequency]}
-              onPressIconRight={() => toggleMedSelection(obj.name)}
-            />
-          ))}
-      </VStack>
+    <View height="$full">
+      <ScrollView p="$4" pb="$16">
+        <VStack space="md">
+          {medicines &&
+            medicines.map((obj) => (
+              <ListCard
+                text={obj.name}
+                isSelected={selectedMeds.includes(obj.name)}
+                iconLeft={CalendarDaysIcon}
+                iconRightOn={CalendarDaysIcon}
+                iconRightOff={SunIcon}
+                badge={[obj.dosage, obj.frequency]}
+                onPressIconRight={() => toggleMedSelection(obj.name)}
+              />
+            ))}
+        </VStack>
+        <View h={120} />
+      </ScrollView>
 
-      <Button isDisabled={selectedMeds.length < 1} onPress={handleSave}>
-        <ButtonText>Save</ButtonText>
-      </Button>
-    </ScrollView>
+      <Box
+        position="absolute"
+        bottom={0}
+        left={0}
+        right={0}
+        p="$4"
+        pb="$8"
+        bg="$white"
+      >
+        <Button isDisabled={selectedMeds.length < 1} onPress={handleSave}>
+          <ButtonText>Save</ButtonText>
+        </Button>
+      </Box>
+    </View>
   );
 };
 
