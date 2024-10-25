3df7ba6a4285e6eb2ee143d4270dce6592066476
Akiodesukedo <abtai0227aki@gmail.com>
2024-10-25 16:26:21 -0700

    build: dynamic dates for streak calendar

--- frontend/src/components/organisms/CalenderStreak.tsx
+++ frontend/src/components/organisms/CalenderStreak.tsx
@@ -1,9 +1,33 @@
+import { useQuery } from "@apollo/client";
 import { View, Text } from "@gluestack-ui/themed";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Calendar } from 'react-native-calendars';
+import { GET_ALL_DATES_WITH_LOG_IN_A_MONTH } from "../../utils/query/badgesScreenQueries";
 
 const CalenderStreak: React.FC = () => {
-  const [selected, setSelected] = useState('');
+
+  const userId = "670db268582e7e887e447288";
+  const [logDates, setLogDates] = useState<any[]>([]);
+  const [markedDates, setMarkedDates] = useState<any[]>([])
+
+  const { loading, error, data } = useQuery(GET_ALL_DATES_WITH_LOG_IN_A_MONTH, {
+    variables: { userId: userId, year: 2024, month: 10 },
+  });
+
+  console.log(data);
+  useEffect(() => {
+    setLogDates(data.getTestResultsDatesByMonth);
+    console.log('logdates', data.getTestResultsDatesByMonth)
+  }, [data])
+
+  useEffect(() => {
+    const formattedDate = logDates.reduce((acc, date) => {
+      acc[date] = { selected: true, selectedColor: 'orange'  }; // Highlighted date with a dot
+      return acc;
+    }, {} as Record<string, any>);
+    setMarkedDates(formattedDate);
+  }, [logDates])
+
 
   return (
     <View
@@ -15,11 +39,11 @@ const CalenderStreak: React.FC = () => {
       padding={10}
     >
       <Calendar 
-        onDayPress={day => {
-          setSelected(day.dateString);
-        }}
+        // onDayPress={day => {
+        //   setSelected(day.dateString);
+        // }}
         markedDates={{
-          [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+          ...markedDates
         }}
       />
     </View>
--- frontend/src/utils/query/badgesScreenQueries.tsx
+++ frontend/src/utils/query/badgesScreenQueries.tsx
@@ -24,3 +24,9 @@ export const GET_ALL_BADGES_INFO_BY_USER = gql`
     }
   }
 `
+
+export const GET_ALL_DATES_WITH_LOG_IN_A_MONTH = gql`
+  query GetAllDates($userId: ID!, $year: Int!, $month: Int!) {
+    getTestResultsDatesByMonth(user_id: $userId, year: $year, month: $month)
+  }
+`
