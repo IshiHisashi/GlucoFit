89dadc85468306d4b34343777672cc2296034fe5
Akiodesukedo <abtai0227aki@gmail.com>
2024-10-29 16:37:09 -0700

    build: integrate scanning device

--- frontend/src/components/screens/logScreens/AutoLogScreen.tsx
+++ frontend/src/components/screens/logScreens/AutoLogScreen.tsx
@@ -6,6 +6,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { HeaderWithBackButton } from "../../headers/HeaderWithBackButton";
 import BluetoothStateManager from 'react-native-bluetooth-state-manager';
 import iHealthAPI from '../../api/iHealthAPI';
+import useScanAPI from '../../api/useScanAPI';
+import useConnectAPI  from '../../api/useConnectAPI';
 import { useEffect, useState } from "react";
 
 type AutoLogScreenNavigationProps = NativeStackNavigationProp<
@@ -16,6 +18,13 @@ type AutoLogScreenNavigationProps = NativeStackNavigationProp<
 const AutoLogScreen: React.FC = () => {
   const navigation = useNavigation<AutoLogScreenNavigationProps>();
   const [bluetoothState, setBluetoothState] = useState<any>(null);
+  const {onScanState, isScanning, scanDevice} = useScanAPI();
+  const {onConnectedState, onConnectFailState, onDisConnectState, connectDevice} = useConnectAPI();
+  const [scanDevices, setScanDevices] = useState<any[]>([]);
+  const type = "BG5S";
+
+  // Authenticate with the iHealth SDK
+  iHealthAPI.sdkAuthWithLicense('com_glucofit_glucofit_ios.pem');
 
   useEffect(() => {
     // Get the initial state of Bluetooth
@@ -28,9 +37,23 @@ const AutoLogScreen: React.FC = () => {
       setBluetoothState(newState);
     }, true);
 
-    // Clean up the subscription on component unmount
+    // Clean up
     return () => subscription.remove();
   }, []);
+
+  useEffect(() => {
+    setScanDevices([]);
+    scanDevice(type);
+  }, [bluetoothState])
+
+  useEffect(() => {
+    if (onScanState?.mac != null) {
+      setScanDevices([
+        ...scanDevices,
+        onScanState
+      ])
+    }
+  }, [onScanState])
 
   return (
     <SafeAreaView>
@@ -45,6 +68,9 @@ const AutoLogScreen: React.FC = () => {
         </Text>
         <Text>
           Bluetooth is {bluetoothState === 'PoweredOn' ? 'ON' : 'OFF'}
+        </Text>
+        <Text>
+          {scanDevices[0]?.mac ? scanDevices[0].mac : "Scanning" }
         </Text>
       </View>
     </SafeAreaView>
