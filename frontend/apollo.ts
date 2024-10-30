69886f990d5e9f56366cc9b8b42e7d37937ad190
Akiodesukedo <abtai0227aki@gmail.com>
2024-10-30 12:34:07 -0700

    build: Integrate all the auto logging features.

--- frontend/apollo.ts
+++ frontend/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://172.20.10.2:3000/graphql",
+  uri: "http://192.168.1.95:3000/graphql",
   cache: new InMemoryCache(),
 });
