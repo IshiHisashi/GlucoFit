b4a23ca9d26eafb21cd7b42d5053bb2911baa0ab
Akiodesukedo <abtai0227aki@gmail.com>
2024-11-06 10:30:54 -0800

    fix: back to localhost

--- frontend/apollo.ts
+++ frontend/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://172.20.10.2:3000/graphql",
+  uri: "http://localhost:3000/graphql",
   cache: new InMemoryCache(),
 });
