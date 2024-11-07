6d003e87f3c3a8649f475697d5f472899f5ec97f
Akiodesukedo <abtai0227aki@gmail.com>
2024-11-06 22:55:52 -0800

    build: to run the app on iPhone

--- frontend/apollo.ts
+++ frontend/apollo.ts
@@ -3,6 +3,6 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
+  uri: "http://192.168.1.95:3000/graphql",
   cache: new InMemoryCache(),
 });
