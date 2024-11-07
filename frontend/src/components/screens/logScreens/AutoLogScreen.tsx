55822ac6f7cc9f9c618ec3483e555c6d1481d7a6
Akiodesukedo <abtai0227aki@gmail.com>
2024-11-07 10:46:35 -0800

    build: integrate all the auto logging flow

--- frontend/src/components/screens/logScreens/AutoLogScreen.tsx
+++ frontend/src/components/screens/logScreens/AutoLogScreen.tsx
@@ -9,6 +9,11 @@ import iHealthAPI from '../../api/iHealthAPI';
 import useScanAPI from '../../api/useScanAPI';
 import useConnectAPI  from '../../api/useConnectAPI';
 import { useEffect, useState } from "react";
+import useDeviceAPI from "../../api/useDeviceAPI";
+import deviceAPIs from '../../api/getAPIs';
+import { Modal, StyleSheet } from "react-native";
+import { Image } from "@gluestack-ui/themed";
+import { Pressable } from "@gluestack-ui/themed";
 
 type AutoLogScreenNavigationProps = NativeStackNavigationProp<
   AppStackParamList,
@@ -20,28 +25,45 @@ const AutoLogScreen: React.FC = () => {
   const {onScanState, isScanning, scanDevice} = useScanAPI();
   const {onConnectedState, onConnectFailState, onDisConnectState, connectDevice} = useConnectAPI();
   const [scanDevices, setScanDevices] = useState<any[]>([]);
+  const { response } = useDeviceAPI();
+  const [parsedRes, setParsedRes] = useState<any>();
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [BGL, setBGL] = useState<number>();
+
   const type = "BG5S";
+
+  // const startMeasuringFunc = Reflect.get(deviceAPIs.getDeviceAPI().apis, 'startMeasure', scanDevices[0].mac);
+
   // Authenticate with the iHealth SDK
   iHealthAPI.sdkAuthWithLicense('com_glucofit_glucofit_ios.pem');
 
+  // Parse response from device
   useEffect(() => {
-    // Get the initial state of Bluetooth
+    if (response !== null && response !== "" ) {
+      const resString = response;
+      const parsedObj = JSON.parse(resString);
+      setParsedRes(parsedObj);
+    }
+  }, [response])
+
+  // Bluetooth state management
+  useEffect(() => {
     BluetoothStateManager.getState().then((state) => {
       setBluetoothState(state);
     });
-    // Listen for changes in the Bluetooth state
     const subscription = BluetoothStateManager.onStateChange((newState) => {
       setBluetoothState(newState);
     }, true);
-    // Clean up 
     return () => subscription.remove();
   }, []);
 
+  // Once bluetooth is on, it start searching a device
   useEffect(() => {
     setScanDevices([]);
     scanDevice(type);
   }, [bluetoothState])
 
+  // Once find a device store the device in scanDevices variable
   useEffect(() => {
     if (onScanState?.mac != null) {
       setScanDevices([
@@ -50,6 +72,37 @@ const AutoLogScreen: React.FC = () => {
       ])
     }
   }, [onScanState])
+
+  // Secure the connection between the device and a phone
+  useEffect(() => {
+    if (scanDevices[0]?.mac != null) {
+      connectDevice(scanDevices[0]?.mac, scanDevices[0]?.type)
+    }
+  }, [scanDevices])
+
+  // Once the device is connected, automatically start measuring
+  useEffect(() => {
+    const startMeasuringFunc = Reflect.get(deviceAPIs.getDeviceAPI().apis, 'startMeasure', onConnectedState.mac);
+    if (onConnectedState.mac) {
+      startMeasuringFunc(onConnectedState.mac);
+    }
+  }, [onConnectedState])
+
+  // Show the modal once it gets blood and starts calculation
+  useEffect(() => {
+    if (parsedRes?.action === 'ACTION_GET_BLOOD' || parsedRes?.action === 'ACTION_RESULT') {
+      setModalVisible(true);
+      if (parsedRes?.action === 'ACTION_RESULT') {
+        const mmolValue = Math.round(parsedRes.RESULT_VALUE * 0.555) / 10
+        setBGL(mmolValue);
+      }
+    }
+  }, [parsedRes])
+
+  const moveToResult = () => {
+    setModalVisible(false)
+    navigation.navigate('GlucoseLog');
+  }
 
 
   return (
@@ -60,17 +113,192 @@ const AutoLogScreen: React.FC = () => {
           text="Log automatically"
           rightIconOnPress={() => {}}
         />
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            // Alert.alert('Modal has been closed.');
+            alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              { parsedRes?.action === 'ACTION_GET_BLOOD' ? 
+                // Calculating modal
+                <View style={styles.viewStyle}>
+                  <Image 
+                    source={require('../../../../assets/autoLogImgs/loading.png')}
+                    style={{width: 30, height: 30}}
+                  />
+                  <Text>
+                    Calculating your blood glucose level...
+                  </Text>
+                </View>
+              : 
+              parsedRes?.action === 'ACTION_RESULT' ? 
+                  // Result modal
+                  <View style={styles.viewStyle}>
+                    <Image 
+                      source={require('../../../../assets/autoLogImgs/gluco-chan.png')}
+                      style={{width: 44, height: 40}}
+                    />
+                    <Text>
+                      { BGL } mmol/L
+                    </Text>
+                    <Text style={styles.modalText}>
+                      Updating your reading...
+                    </Text>                  
+                  </View>
+                :
+                  // Error modal for measurement
+                  <View style={styles.viewStyle}>
+                    <Image 
+                      source={require('../../../../assets/autoLogImgs/error-measure.png')}
+                      style={{width: 30, height: 30}}
+                    />
+                    <Text>
+                      Oops! We couldn't process your reading.The test strip may be contaminated or not have enough blood sample. Please try again.
+                    </Text>
+                  </View>
+              }
+              
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => moveToResult()}>
+                <Text style={styles.textStyle}>Next</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        { parsedRes?.action === 'ACTION_STRIP_IN' || parsedRes?.action === 'ACTION_GET_BLOOD' || parsedRes?.action === 'ACTION_RESULT' ?
+          // Third Screen
+          <View style={styles.viewStyle}>
+            <Image 
+              source={require('../../../../assets/autoLogImgs/ready-to-measure.png')}
+              style={{width: 200, height: 200}}
+            />
+            <Text>
+              Strip is in place. Waiting for your blood sample.
+            </Text>
+          </View>
+        :
+          bluetoothState === 'PoweredOn' && scanDevices[0]?.mac && onConnectedState.mac ?
+            parsedRes?.action === 'ACTION_STRIP_OUT' ? 
+              // Strip Out Screen
+              <View style={styles.viewStyle}>
+                <Image 
+                  source={require('../../../../assets/autoLogImgs/strip-error.png')}
+                  style={{width: 200, height: 200}}
+                />
+                <Text>
+                  Strip is not inserted properly.
+                </Text>
+                <Text>
+                  Please insert it again.
+                </Text>
+              </View>
+            :
+              parsedRes?.action === 'ACTION_ERROR_BG' && parsedRes.ERROR_NUM_BG === 3 ?
+                // Strip already used Screen
+                <View style={styles.viewStyle}>
+                  <Image 
+                    source={require('../../../../assets/autoLogImgs/strip-error.png')}
+                    style={{width: 200, height: 200}}
+                  />
+                  <Text>
+                    Strip is already used or unknown moisture detected, discard the test strip and repeat the test with a new strip.
+                  </Text>
+                </View>
+              :
+                // Second Screen
+                <View style={styles.viewStyle}>
+                  <Image 
+                    source={require('../../../../assets/autoLogImgs/insert-strip.png')}
+                    style={{width: 200, height: 200}}
+                  />
+                  <Text>
+                    Insert test strip in the glucometer and prepare your blood sample.
+                  </Text>   
+                </View>
+          :
+            // First Screen
+            <View style={styles.viewStyle}>
+              <Image 
+                source={require('../../../../assets/autoLogImgs/connect-device.png')}
+                style={{width: 200, height: 200}}
+              />
+              <Text>
+                Make sure the bluetooth is turned on and the glucometer is nearby.
+              </Text>  
+            </View>
+        }
         <Text>
-          This is Auto Log Screen.
-        </Text>
-        <Text>
-          Bluetooth is {bluetoothState === 'PoweredOn' ? 'ON' : 'OFF'}
-        </Text>
-        <Text>
-          {scanDevices[0]?.mac ? scanDevices[0].mac : "Scanning" }
+          { parsedRes?.action }
+          { parsedRes?.RESULT_VALUE }
         </Text>
       </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  viewStyle: {
+    alignItems: 'center',
+  },
+  buttonStyle: {
+    backgroundColor: '#2089dc',
+    borderColor: 'transparent',
+    borderWidth: 0,
+    borderRadius: 10,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+})
+
 export default AutoLogScreen
