e167a6ab9bb347ccdce4f2bb73589ffa89414e86
IshiHisashi <ihsasihhi@gmail.com>
2024-11-21 22:27:00 -0800

    add splash

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,5 @@
 import { GluestackUIProvider } from "@gluestack-ui/themed";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 // import { config } from "@gluestack-ui/config";
@@ -13,15 +13,42 @@ import extendedConfig from "./theme.config";
 import { AuthProvider } from "./src/context/AuthContext";
 import RootStack from "./src/components/stacks/RootStack";
 
+import LottieView from "lottie-react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
 const App: React.FC = () => {
+  const [isSplashVisible, setSplashVisible] = useState<boolean>(true);
   const [fontsLoaded] = useFonts({
     "NunitoSans-Regular": require("./assets/fonts/NunitoSans_10pt-Regular.ttf"),
     "NunitoSans-Bold": require("./assets/fonts/NunitoSans_10pt-Bold.ttf"),
     "NunitoSans-SemiBold": require("./assets/fonts/NunitoSans_10pt-SemiBold.ttf"),
   });
 
-  if (!fontsLoaded) {
-    return null; // or a loading screen
+  useEffect(() => {
+    const prepareApp = async () => {
+      if (!fontsLoaded) return;
+
+      await new Promise((resolve) => setTimeout(resolve, 3500));
+      setSplashVisible(false);
+      SplashScreen.hideAsync();
+    };
+
+    prepareApp();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded || isSplashVisible) {
+    return (
+      <SafeAreaProvider>
+        <LottieView
+          source={require("./src/components/animations/logo-animation.json")}
+          autoPlay
+          loop={false}
+          style={{ flex: 1, backgroundColor: "#4800FF" }}
+        />
+      </SafeAreaProvider>
+    );
   }
 
   return (
@@ -30,8 +57,6 @@ const App: React.FC = () => {
         <ApolloProvider client={client}>
           <AuthProvider>
             <RootStack />
-            {/* <StatusBar style="auto" /> */}
-            {/* <AppStack /> */}
           </AuthProvider>
         </ApolloProvider>
       </GluestackUIProvider>
