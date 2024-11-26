a1bbbd42de7a41ba50e1b95a8ad004224d9121fb
IshiHisashi <ihsasihhi@gmail.com>
2024-11-26 15:37:54 -0800

    some changes

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -22,7 +22,7 @@ type AuthProviderProps = {
 
 const client = new ApolloClient({
   // uri: "http://192.168.1.65:3000/graphql",
-  uri: "http://ec2-34-224-127-225.compute-1.amazonaws.com:3000/graphql",
+  uri: "https://ec2-34-224-127-225.compute-1.amazonaws.com:3000/graphql",
   cache: new InMemoryCache(),
 });
 
