80ac7887c47ccfaa275b9a92aae484cc06ad4d8b
IshiHisashi <ihsasihhi@gmail.com>
2024-11-26 23:38:53 -0800

    change url of endpoint

--- frontend/apollo.ts
+++ frontend/apollo.ts
@@ -4,6 +4,6 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
   // uri: "http://192.168.1.65:3000/graphql",
-  uri: "https://ec2-34-224-127-225.compute-1.amazonaws.com:3000/graphql",
+  uri: "https://34.224.127.225/graphql",
   cache: new InMemoryCache(),
 });
